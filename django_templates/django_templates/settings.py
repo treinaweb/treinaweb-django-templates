13f46564be40288074fdca73b55df176bc15fa78
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-03-14 14:51:35 -0300

    10.05_Configurando_Aplicacao

--- django_templates/django_templates/settings.py
+++ django_templates/django_templates/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,7 +105,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt'
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('pt', _('Portuguese')),
+    ('ru', _('Russian')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 TIME_ZONE = 'UTC'
 
