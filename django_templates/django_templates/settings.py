6a2b6b597f821a4ab77dd23ad9868fd715d62c93
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-03-14 15:33:34 -0300

    10.07_Alterando_Idioma_Execucao

--- django_templates/django_templates/settings.py
+++ django_templates/django_templates/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt'
+LANGUAGE_CODE = 'ru'
 
 LANGUAGES = (
     ('en', _('English')),
--- django_templates/django_templates/urls.py
+++ django_templates/django_templates/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls'))
 ]
+
+urlpatterns += i18n_patterns(
+    path('app/', include('app.urls'))
+)
+
+
